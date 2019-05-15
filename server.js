469ebcecf8a8b55269444825896db3c50ab4962e
Abdullah-Alharbi <w2.alharbi.me@gmail.com>
2019-05-15 04:03:08 +0300

    documented

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+/**
+ * this file basically is the entry point of
+ * the app and it's create http server
+ * and load the middlewares some middlewares
+ * will be executed between the request and the response
+ * after that will load the routes
+ * lastly create the http server
+ */
 const express = require('express');
 const { port, host } = require('./config.json');
 
 const app = express();
 
-// this will load all the routes
-app.use(require('./app/routes'));
-// this will load all the middlewares
 app.use(require('./app/middlewares'));
 
-// eslint-disable-next-line no-console
+app.use(require('./app/routes'));
+
 app.listen(port, host, () => console.log(`Listening to http://${host}:${port}`));
