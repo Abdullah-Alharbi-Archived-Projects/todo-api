218112e9b5adcc985f10d55d941e0e6e19603440
Abdullah-Alharbi <w2.alharbi.me@gmail.com>
2019-05-15 04:31:00 +0300

    documented

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,14 @@
+/**
+ * this file will be dictionary for all
+ * the routes first it's create new router
+ * then it's call the method that in the controller
+ * and lastly export that router to be used in the server.js file
+ */
 const { Router } = require('express');
 
 const router = new Router();
 
-// require the controllers here
+// all the controllers goes here
 const mainController = require('./controllers/mainController');
 
 // all the routes goes here
