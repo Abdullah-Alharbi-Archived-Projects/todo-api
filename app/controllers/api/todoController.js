dc823fb45934e1ab55960d4a28512d8d3c3ef2ce
Abdullah-Alharbi <w2.alharbi.me@gmail.com>
2019-05-15 05:26:09 +0300

    added store method

--- app/controllers/api/todoController.js
+++ app/controllers/api/todoController.js
@@ -1,4 +1,5 @@
 const TodoModel = require('../../models/Todo');
+const { destructTodo } = require('../../helpers/object');
 
 module.exports = {
   index(req, res) {
@@ -6,6 +7,16 @@ module.exports = {
       .find()
       .sort({ date: -1 })
       .then(todos => res.status(200).json(todos))
+      .catch(err => console.log(err));
+  },
+  store(req, res) {
+    const newTodo = new TodoModel({
+      name: req.body.name,
+    });
+
+    newTodo
+      .save()
+      .then(todo => res.status(201).json({ message: 'saved!', ...destructTodo(todo) }))
       .catch(err => console.log(err));
   },
 };
