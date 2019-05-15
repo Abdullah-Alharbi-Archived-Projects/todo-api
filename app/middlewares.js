d6305be9fe354655fdc1bcc53e098402aa570196
Abdullah-Alharbi <w2.alharbi.me@gmail.com>
2019-05-15 06:23:59 +0300

    formatted!

--- app/middlewares.js
+++ app/middlewares.js
@@ -1,14 +1,17 @@
 const { Router } = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 
 const router = new Router();
 
 
 // use any middleware here
-router.use(morgan('dev'));
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
+router
+	.use(morgan('dev'))
+	.use(helmet())
+	.use(bodyParser.json())
+	.use(bodyParser.urlencoded({ extended: false }));
 
 
 module.exports = router;
