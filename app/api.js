727a082a4308690662fd4c67b6277c54e0143be2
Abdullah-Alharbi <w2.alharbi.me@gmail.com>
2019-05-15 05:24:25 +0300

    added post verb for todo route

--- app/api.js
+++ app/api.js
@@ -3,11 +3,33 @@ const { Router } = require('express');
 const router = new Router();
 
 const mainController = require('./controllers/api/mainController');
+const todoController = require('./controllers/api/todoController');
 
-router.get('/', mainController.index); // /api/
+
+router
+  /**
+   * @route  GET /api/
+   * @desc   welcome message
+   * @access Public
+   */
+  .get('/', mainController.index);
 
 
 // todo routes here
+router
+  /**
+   * @route  GET /api/todo/
+   * @desc   get all the todos
+   * @access Public
+   */
+  .get('/todo/', todoController.index)
+
+  /**
+   * @route  POST /api/todo/
+   * @desc   create new todo
+   * @access Public
+   */
+  .post('/todo/', todoController.store);
 
 
 module.exports = router;
