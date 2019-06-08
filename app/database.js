d0d76ada56b643882b9badd17d555085043ddba5
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-06-08 03:48:27 +0300

    remove try/catch block

--- app/database.js
+++ app/database.js
@@ -3,19 +3,14 @@
  * and export the connection to be used across the project
  */
 const mongoose = require('mongoose');
-const { mongoURI } = require('./config/keys'); // this is the mongodb string
 
-try {
-  mongoose
-    // 1- try to connect to the database
-    .connect(mongoURI, { useNewUrlParser: true })
-    // 2- if the connect have been resolved print that the database connected successfully
-    .then(() => console.log('Database Connected Successfully!'))
-    // 3- otherwise if the connect have been rejected print the error
-    .catch(err => console.log('Unable to connect', err));
-} catch (O_O) {
-  console.log(O_O);
-}
+mongoose
+  // 1- try to connect to the database
+  .connect('mongodb://localhost/todo', { useNewUrlParser: true, useFindAndModify: true })
+  // 2- if the connect have been resolved print that the database connected successfully
+  .then(() => console.log('Database Connected Successfully!'))
+  // 3- otherwise if the connect have been rejected print the error
+  .catch(err => console.log('Unable to connect', err));
 
 // export the mongoose object to use in other files
 module.exports = mongoose;
